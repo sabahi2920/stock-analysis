541d02f1ef442c852b9a7e6d2377a58ce5542ad3
sabahi2920 <76417748+sabahi2920@users.noreply.github.com>
2021-11-22 18:56:58 -0600

    initial commit

---
+++ includes/dbconn.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>DBConn Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
+
+<!--$dbserver = "localhost";-->
+<!--$dbusername = "stockanalysis_root";-->
+<!--$dbpass = "%2Y&?JfcO-u$";-->
+<!--$dbname = "stockanalysis_db";-->
+
+<!--$conn = mysqli_connect($dbserver, $dbusername, $dbpass, $dbname);-->
+<!--$sql = "SELECT * FROM portfolio;";-->
+
+<!--$result = mysqli_query($conn, $sql);-->
+<!--while($row = mysqli_fetch_assoc($result)){-->
+<!--    echo $row['userName'] . "<br>";-->
+<!--}-->
